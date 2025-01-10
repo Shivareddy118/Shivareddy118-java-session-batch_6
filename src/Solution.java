7b201f7fe878b6a6198addf7926494d7a658ecec
Shivareddy118 <2311cs010177@mallareddyuniversity.ac.in>
2025-01-10 12:40:44 +0530

    first

---
+++ src/Solution.java
@@ -0,0 +1,23 @@
+class day_7Solution {
+    public boolean containsDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        day_7Solution solution = new day_7Solution();
+        int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+        System.out.println(solution.containsDuplicate(nums1)); 
+        System.out.println(solution.containsDuplicate(nums2)); 
+        System.out.println(solution.containsDuplicate(nums3));
+    }
+}
---
+++ src/day_6.java
@@ -0,0 +1,16 @@
+public class day_6 {
+    public static void main(String[] args) {
+        for(int i =0; i < 4; i++){           //outer loop of rows
+            for(int j = 0; j < 4; j++){      // inner loop of columns
+                System.out.print("y");
+            }
+            System.out.println();
+        }
+        for(int i =0; i < 4; i++){           //outer loop of rows
+            for(int j = 0; j < 4; j++){      // inner loop of columns
+                System.out.print("y");
+            }
+            System.out.println();
+        }
+    }
+}
